a4191bb0aced4c72e88b08ec253e5e98be9253ed
Taruna <tarunajindal1998@gmail.com>
2019-02-20 21:14:19 +0530

    changed again

--- main.js
+++ main.js
@@ -26,3 +26,51 @@
   $('#datepicker3').datepicker({
             uiLibrary: 'bootstrap4'
         });
+
+  //correspondance address same as permanent address
+  function auto_fill_address(){
+    var same_addr = document.getElementById("defaultCheck1").checked;
+    var pnationality = document.getElementById("p_nationality").value;
+    var pstate = document.getElementById("p_state").value;
+    var pcity = document.getElementById("p_city").value;
+    var pdistrict = document.getElementById("p_district").value;
+    var pcountry = document.getElementById("p_country").value;
+    var ppincode = document.getElementById("p_pincode").value;
+    var paddress = document.getElementById("p_address").value;
+    //alert(x);
+    if(same_addr){
+     if((pnationality=='' || pnationality==null)||(pstate=='' || pstate==null)||(pcity=='' || pcity==null)||(pdistrict=='' || pdistrict==null)||(pcountry=='' || pcountry==null)||(ppincode=='' || ppincode==null)||(paddress=='' || paddress==null)){
+      alert('please fill permanent address');
+      document.getElementById("same_residential_permanent").checked = false;
+     }else{
+      document.getElementById("c_nationality").value = pnationality;
+      document.getElementById("c_state").value = pstate;
+      document.getElementById("c_city").value = pcity;
+      document.getElementById("c_district").value = pdistrict;
+      document.getElementById("c_country").value = pcountry;
+      document.getElementById("c_pincode").value = ppincode;
+      document.getElementById("c_address").value = paddress;
+     }
+    }
+    else
+    {
+      document.getElementById("c_nationality").value = "";
+      document.getElementById("c_state").value = "";
+      document.getElementById("c_city").value = "";
+      document.getElementById("c_district").value = "";
+      document.getElementById("c_country").value = "";
+      document.getElementById("c_pincode").value = "";
+      document.getElementById("c_address").value = "";
+    }  
+     }
+  $(function() {
+    unlock_form();
+      $("#defaultCheck2").click(unlock_form);
+    });
+  function unlock_form(){
+   if (this.checked) {
+    $("input.defaultCheck2").removeAttr("disabled");
+  } else {
+    $("input.defaultCheck2").attr("disabled", true);
+  }
+  }
